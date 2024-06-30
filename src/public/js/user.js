ddaeb1ba518b3ea867f66308587e37036286499b
Ranit Jana <ranitjana100@gmail.com>
2024-06-30 16:44:54 +0530

    fix responsive bug

--- src/public/js/user.js
+++ src/public/js/user.js
@@ -40,6 +40,48 @@ document.addEventListener('DOMContentLoaded', function () {
 
 })
 
+
+let mode = document.querySelector(".mode");
+
+
+function setTheme(theme) {
+
+    document.documentElement.setAttribute('data-theme', theme);
+
+    if (theme == 'dark') {
+        document.querySelector(".mode img").setAttribute("src", "/images/icons8-sun-90.png")
+    }
+    else {
+        document.querySelector(".mode img").setAttribute("src", "/images/icons8-dark-mode-100.png")
+    }
+
+    localStorage.setItem('theme', theme);
+}
+
+
+function toggleTheme() {
+
+    const currentTheme = localStorage.getItem('theme');
+
+    let newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+
+    setTheme(newTheme);
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    let theme = localStorage.getItem('theme') || 'light';
+
+    setTheme(theme);
+});
+
+
+mode.addEventListener("click", function () {
+    toggleTheme();
+})
+
+
 let Blogger = document.querySelector("header .left h1");
 
 function textAdjust() {
