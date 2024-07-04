231ccf9e4de9e2e9bb4ccf1b233fcbbb547999ab
Ranit Jana <ranitjana100@gmail.com>
2024-07-05 00:34:16 +0530

    fix __dirname after changing to module js

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -1,5 +1,8 @@
 import multer from "multer";
 import path from "path";
+import { fileURLToPath } from "url"
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
